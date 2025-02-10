7ea452ee3229500b1db910c1754c51ea139ee0a2
HaydenHartmann1 <156471807+HaydenHartmann1@users.noreply.github.com>
2025-02-10 09:15:25 -0600

    First Commit
    The Password Validator level 24

---
+++ The_Password_Validator_lvl_24/The_Password_Validator_lvl_24/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+    Hayden Hartmann
+    2/5/2025
+    The Password Validator 
+*/
+
+
+public class ThePasswordValidator
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("Do you wish to create passwords? (y/n): ");
+        string user = Console.ReadLine();
+
+        bool _bool = false;
+        if (user == "y")
+        {
+            _bool = true;
+        }
+
+        while (_bool)
+        {
+            Console.Write("Enter A Password: ");
+            string password = 
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
