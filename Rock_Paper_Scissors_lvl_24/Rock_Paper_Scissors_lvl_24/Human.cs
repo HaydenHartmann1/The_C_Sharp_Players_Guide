baaa9be5adc66c29e290634cc4f8eeb526b9710c
HaydenHartmann1 <156471807+HaydenHartmann1@users.noreply.github.com>
2025-02-10 09:15:54 -0600

    First Commit
    Rock Paper Scissors

---
+++ Rock_Paper_Scissors_lvl_24/Rock_Paper_Scissors_lvl_24/Human.cs
@@ -0,0 +1,27 @@
+﻿
+// This class is only for creating a person that has a Name for the game (Rock Paper Scissors)
+public class Human
+{
+    private string _name = "n/a";
+    private Game _choice;
+
+    public Human(string name)
+    {
+        _name = name;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    //pointer sets the _choice to be one of the Game enum values
+    public Game Choice
+    {
+        get => _choice;
+        set => _choice = value;
+    }
+}
+
+public enum Game { Rock, Paper, Scissors}
---
+++ Rock_Paper_Scissors_lvl_24/Rock_Paper_Scissors_lvl_24/Program.cs
@@ -0,0 +1,134 @@
+﻿/*
+    Hayden Hartmann
+    2/5/2025
+    Rock Paper Scissors Game
+*/
+
+
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        //human objects
+        Human human1 = new Human("Hayden");
+        Human human2 = new Human("Dylan");
+        
+        //game loop
+        while (true)
+        {
+            //human1 gives response, put through checker method and then assigner. If checker returns false, game restarts
+            Console.Write($"{human1.Name} pick rock, paper, scissors: ");
+            string? choice1 = Console.ReadLine();
+            if (Checker(choice1))
+            {
+                Assigner(choice1, human1);
+            }
+            else
+            {
+                continue;
+            }
+
+            //code above copied, probobly can make a method to reduce code...
+            Console.Write($"{human2.Name} pick rock, paper, scissors: ");
+            string? choice2 = Console.ReadLine();
+            if (Checker(choice2))
+            {
+                Assigner(choice2, human2);
+            }
+            else
+            {
+                continue;
+            }
+
+            //Checkwin method just prints who wins 
+            CheckWin(human1, human2);
+            Console.WriteLine("\n---------------------------------------");
+        }
+
+        Console.ReadKey();
+    }
+
+    //method checks if user input is either rock, paper, or scissors 
+    public static bool Checker(string choice)
+    {
+        if (choice != "rock" && choice != "paper" && choice != "scissors")
+        {
+            Console.WriteLine("You must pick rock, paper, or scissors!\n");
+            Console.WriteLine("---------------------------------------\n");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    //method assigns human.Choice to a Grade enum
+    public static void Assigner(string choice, Human human)
+    {
+        switch (choice)
+        {
+            case "rock":
+                human.Choice = Game.Rock;
+                break;
+            case "paper":
+                human.Choice = Game.Paper;
+                break;
+            case "scissors":
+                human.Choice = Game.Scissors;
+                break;
+        }
+    }
+
+    //method checks win condition of Rock Paper Scissors
+    public static void CheckWin(Human human1, Human human2)
+    {
+        //If both are equal. Simplifies code. Otherwise simple if else block to test human.Choices against each other
+        if (human1.Choice == human2.Choice)
+        {
+            Console.WriteLine("Its a tie!");
+        }
+        else
+        {
+            if (human1.Choice == Game.Paper)
+            {
+                if (human2.Choice == Game.Rock)
+                {
+                    Console.WriteLine($"{human1.Name} wins!");
+                }
+                else
+                {
+                    Console.WriteLine($"{human2.Name} wins!");
+                }
+            }
+            else if (human1.Choice == Game.Rock)
+            {
+                if (human2.Choice == Game.Paper)
+                {
+                    Console.WriteLine($"{human2.Name} wins!");
+                }
+                else
+                {
+                    Console.WriteLine($"{human1.Name} wins!");
+                }
+            }
+            else if (human1.Choice == Game.Scissors)
+            {
+                if (human2.Choice == Game.Rock)
+                {
+                    Console.WriteLine($"{human2.Name} wins!");
+                }
+                else
+                {
+                    Console.WriteLine($"{human1.Name} wins!");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
